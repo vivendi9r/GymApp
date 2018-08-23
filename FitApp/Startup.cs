230e7007e1006bcac1a790aa730dc4b162609f8a
vivendi9r <vivendi9r@gmail.com>
2018-08-23 13:36:24 +0200

    Dodaj pliki projektów.

---
+++ FitApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitApp.Startup))]
+namespace FitApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
