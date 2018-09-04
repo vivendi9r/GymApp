1cbcb651f0cfb6cc43c5214e94f8642c47a59aa0
vivendi9r <vivendi9r@gmail.com>
2018-09-04 09:32:38 +0200

    add content to subpages

--- FitApp/Controllers/HomeController.cs
+++ FitApp/Controllers/HomeController.cs
@@ -43,14 +43,14 @@ namespace FitApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+           
 
             return View();
         }
